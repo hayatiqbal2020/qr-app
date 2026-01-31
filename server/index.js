87c448bfe165637ccc202a4b7838451f0c0455cb
hayatiqbal2020-labra <iqbal.h@labra.io>
2026-02-01 00:35:01 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://qr-app-red.vercel.app'],
   credentials: true,
 }))
 app.use(express.json())
